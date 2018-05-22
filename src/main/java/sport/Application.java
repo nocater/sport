323fb562256f66a09cac65a5d8d0399e2113bde7
Sleepincoffee <cs781503542@qq.com>
2018-05-22 12:47:44 +0800

    sport项目初始化

---
+++ src/main/java/sport/Application.java
@@ -0,0 +1,19 @@
+package sport;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 		//same as @Configuration @EnableAutoConfiguration @ComponentScan
+@MapperScan("sport.dao")	//扫描包下文件
+//@Slf4j					//日志
+public class Application {
+	private final static Logger log = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		log.info("===Sport started!===");
+	}
+}
