5b79ca5c1eae25664078b145641c61f453fc73ee
nocater <nocater@163.com>
2018-05-23 09:21:39 +0800

    完成登录 及 Session的设置

--- src/main/java/sport/web/control/LoginCtrl.java
+++ src/main/java/sport/web/control/LoginCtrl.java
@@ -1,16 +1,43 @@
 package sport.web.control;
 
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class LoginCtrl {
-
+	
+	/***
+	 * 登录
+	 * session 写入DEMO
+	 * @param name
+	 * @param password
+	 * @param session
+	 * @return
+	 */
 	@RequestMapping(value="/login")
 	public String login(@RequestParam String name,
-						@RequestParam String password){
+						@RequestParam String password,
+						HttpSession session
+						){
 		System.out.println(name+" "+password);
-		return "login";
+		session.setAttribute("LOGIN_ACCOUNT", name);
+		return "index";
+	}
+	
+	/***
+	 * 测试Session获取值
+	 * @param LOGIN_ACCOUNT
+	 * @return
+	 */
+	@RequestMapping(value="/logintest")
+	public String login_test(@SessionAttribute String LOGIN_ACCOUNT
+						){
+		System.out.println("已获取Session 中LOGIN_ACCOUNT："+LOGIN_ACCOUNT);
+		return "index";
 	}
 }
--- src/main/java/sport/web/control/SchoolCtrl.java
+++ src/main/java/sport/web/control/SchoolCtrl.java
@@ -1,10 +1,19 @@
 package sport.web.control;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 
 @Controller
-//@RequestMapping(value="/school")
+@RequestMapping(value="/school")
 public class SchoolCtrl {
 	/***
 	 * info
@@ -13,5 +22,27 @@ public class SchoolCtrl {
 	@RequestMapping("/info")
 	public String index(){
 		return "/school/schoolinfo";
+	}
+	
+	@RequestMapping("/list")
+	public String list(ModelMap model, 
+						@RequestParam(value="pageNum",required=false,defaultValue="1") Integer pageNum,
+						@RequestParam(value = "pageSize", required=false, defaultValue="3") Integer pageSize,
+						@RequestParam(value = "message", required=false) String message
+						){
+		PageHelper.startPage(2, 3);
+		List<String> list = Arrays.asList(new String[]{"燕山大学", "秦皇岛一中",
+				"秦皇岛一中",
+				"秦皇岛2中",
+				"秦皇岛3中",
+				"秦皇岛4中",
+				"秦皇岛5中",
+				"秦皇岛6中",
+				"秦皇岛7中",
+				"秦皇岛8中",
+				"秦皇岛9中"});
+		PageInfo<String> page = new PageInfo<String>(list);
+		model.addAttribute("page", page);
+		return "/school/schoollist";
 	}
 }
